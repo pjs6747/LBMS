1413707e27b89c56d1c26c90dab3514231114971
pjs6747 <pjs6747@rit.edu>
2019-02-27 19:58:18 -0500

    Visitor Class Complete

---
+++ src/Visitor.java
@@ -0,0 +1,78 @@
+public class Visitor{
+
+  /**
+   * First Name of Visitor
+   */
+  private String firstName;
+
+  /**
+   * Last Name of Visitor
+   */
+  private String lastName;
+
+  /**
+   * Address of Visitor
+   */
+  private String address;
+
+  /**
+   * Phone Number of Visitor
+   */
+  private int phoneNumber;
+
+  /**
+   * Unique 10 digit visitor ID
+   */
+  private int visitorID;
+
+
+  public void Visitor(String firstName, String lastName, String address, int phoneNumber, int visitorID){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.visitorID = visitorID;
+  }
+
+  /**
+   * Gets firstName
+   * @return visitor firstName
+   */
+  public String getFirstName() {
+    return firstName;
+  }
+
+  /**
+   * Gets lastName
+   * @return visitor lastName
+   */
+  public String getLastName() {
+    return lastName;
+  }
+
+  /**
+   * Gets address
+   * @return visitor Address
+   */
+  public String getAddress() {
+    return address;
+  }
+
+  /**
+   * Gets phoneNumber
+   * @return visitor phoneNumber
+   */
+  public int getPhoneNumber(){
+    return phoneNumber;
+  }
+
+  /**
+   * Gest ID
+   * @return visitorID
+   */
+  public int getVisitorID(){
+    return visitorID;
+  }
+
+
+}
